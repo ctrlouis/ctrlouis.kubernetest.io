d716455e72773f7ea6074b3363780a31a81d6b00
ctrlouis <louis.bty57@gmail.com>
2020-06-19 13:25:24 +0200

    Add route to get pods informations

--- simple-api/app.js
+++ simple-api/app.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
     res.send('Hello, I\'m the new version of the app')
 });
 
+app.get('/pods', (req, res) => {
+    const infos = {
+        node_name: process.env.MY_NODE_NAME,
+        pod_name: process.env.MY_POD_NAME,
+        pod_namespace: process.env.MY_POD_NAMESPACE,
+        pod_ip: process.env.MY_POD_IP,
+        pod_service_account: process.env.MY_POD_SERVICE_ACCOUNT
+    };
+    res.json(infos);
+});
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
 });
