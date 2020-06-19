8f9daa2fd5831f8878f135527f93bf425e743866
Nekika <hugo.jahnke@gmail.com>
2020-06-19 19:06:46 +0200

    Creating Message component

---
+++ message-client/src/components/Message.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ text }}</p>
+    <p><strong>{{ user }}</strong> - <em>{{ date }}</em></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: {
+      message: { type: Object, default: () => {} }
+  },
+  computed: {
+      user () { return this.message.username || 'anonym' },
+      text () { return this.message.text },
+      date () { return this.message.date },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
