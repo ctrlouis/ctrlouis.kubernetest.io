954ec673d1296f4330a3be9aca9b755e5c20a044
Nekika <hugo.jahnke@gmail.com>
2020-06-19 19:08:37 +0200

    Creating App component - API call : when mounted, performs a GET request to the API in order to retrieve messages - Listing : use the Message component to display the list of messages

---
+++ message-client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <Message
+          v-for="message in messages"
+          :key="message.id"
+          :message="message"
+      />
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Message from '@/components/Message'
+
+export default {
+  name: 'App',
+  components: { Message },
+  data () {
+      return {
+          url: 'http://51.15.254.190:31795/messages',
+          messages: []
+      }
+  },
+  async mounted() {
+      const res = await axios.get(this.url)
+      this.messages = res.data
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
