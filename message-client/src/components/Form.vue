16dd11e7d6a8a184b0af100e4b400af89ae4caea
Nekika <hugo.jahnke@gmail.com>
2020-06-22 10:21:53 +0200

    Form refacto & style

--- message-client/src/components/Form.vue
+++ message-client/src/components/Form.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <label for="ipt-text">Message</label>
-    <input id="ipt-text" type="text" v-model="message.text">
-    <label for="ipt-username">Usermane</label>
-    <input id="ipt-username" type="text" v-model="message.username">
-    <input type="button" value="Send" @click="sendButtonClicked">
-  </div>
+  <form @submit.prevent="sendButtonClicked">
+    <div class="group">
+      <label for="ipt-text">Message</label>
+      <textarea id="ipt-text" v-model="message.text" required></textarea>
+    </div>
+    <div class="group">
+      <label for="ipt-username">Usermane</label>
+      <input id="ipt-username" type="text" v-model="message.username">
+    </div>
+    <input type="submit" value="Send">
+  </form>
 </template>
 
 <script>
@@ -14,11 +18,48 @@ export default {
     message: { type: Object, default: () => { return { username: '', text: '' } } }
   },
   methods: {
-    sendButtonClicked () { this.$emit('sendButtonClicked', this.message) }
+    sendButtonClicked (e) {
+        e.preventDefault()
+        this.$emit('sendButtonClicked', this.message)
+    }
   }
 }
 </script>
 
 <style scoped>
-
+form {
+  align-self: center;
+}
+form, .group {
+  display: flex;
+  flex-direction: column;
+}
+.group {
+  width: 70%;
+  max-width: 400px;
+  margin: 10px auto;
+}
+label, input, textarea {
+  text-align: center;
+}
+input, textarea {
+  border: solid 1px black;
+  border-radius: 2px;
+}
+textarea {
+  resize: none;
+  height: 150px;
+}
+input {
+  padding: 10px 0;
+}
+input[type="submit"] {
+  width: 60px;
+  margin: auto;
+  background-color: #00990077;
+}
+input[type="submit"]:hover {
+  cursor: pointer;
+  background-color: #009900AA;
+}
 </style>
