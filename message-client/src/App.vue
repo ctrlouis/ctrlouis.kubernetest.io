9430f5af4dff881a66cc48c1ef337cf29d1d2f22
Nekika <hugo.jahnke@gmail.com>
2020-06-22 09:33:39 +0200

    Handling message - Creating message property - Adding Form component and passing message prop - Creating sendMessage method

--- message-client/src/App.vue
+++ message-client/src/App.vue
@@ -1,25 +1,37 @@
 <template>
-    <div>
+    <div id="app">
       <Message
           v-for="message in messages"
           :key="message.id"
           :message="message"
       />
+      <Form :message="message" @sendButtonClicked="sendMessage" />
     </div>
 </template>
 
 <script>
 import axios from 'axios'
 import Message from '@/components/Message'
+import Form from '@/components/Form'
 
 export default {
   name: 'App',
-  components: { Message },
+  components: { Message, Form },
   data () {
       return {
           url: 'http://51.15.254.190:31795/messages',
-          messages: []
+          messages: [],
+          message: { author: '', username: ''}
       }
+  },
+  methods: {
+    async sendMessage () {
+        const res = await axios.post(this.url, this.message)
+        if (res.status >= 200 && res.status < 300) {
+            this.messages.push(this.message)
+            this.message = { username: '', text: '' }
+        }
+    }
   },
   async mounted() {
       const res = await axios.get(this.url)
