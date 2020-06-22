7f2fa33345605dbe39d1f61e9edd4576bd080e78
Nekika <hugo.jahnke@gmail.com>
2020-06-22 09:32:36 +0200

    Creating Form component

---
+++ message-client/src/components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <label for="ipt-text">Message</label>
+    <input id="ipt-text" type="text" v-model="message.text">
+    <label for="ipt-username">Usermane</label>
+    <input id="ipt-username" type="text" v-model="message.username">
+    <input type="button" value="Send" @click="sendButtonClicked">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: { type: Object, default: () => { return { username: '', text: '' } } }
+  },
+  methods: {
+    sendButtonClicked () { this.$emit('sendButtonClicked', this.message) }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
