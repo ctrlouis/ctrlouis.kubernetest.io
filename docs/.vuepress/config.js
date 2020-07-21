d6e2ac7e4b7310481f09425c3ba66115748b16c2
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-21 16:44:14 +0200

    docs: Vuepress config Title, description, navbar, sidebar and smoothscroll

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Kubernetest',
+    description: 'Ce rapport présentera les travaux de recherches et de compréhension de Kubernetes.',
+    themeConfig: {
+        nav: [
+            { text: 'Accueil', link: '/'},
+            { text: 'Kubernetes', link: '/kubernetes'}
+        ],
+        sidebar: 'auto',
+        smoothScroll: true
+    }
+}
